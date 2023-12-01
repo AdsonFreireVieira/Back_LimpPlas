362c3d0dceb87554cb53fcbdd91c63d1fbe5036a
Adson <adsonfr.vieira@hotmail.com>
2023-11-30 21:46:09 -0300

    initial Commit

---
+++ backLimpPlas/src/main/java/com/back_LimpPlast/BackLimpPlasApplication.java
@@ -0,0 +1,13 @@
+package com.back_LimpPlast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackLimpPlasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackLimpPlasApplication.class, args);
+	}
+
+}
