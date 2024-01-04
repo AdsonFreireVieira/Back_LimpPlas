8aaa9a90ae430f48e8137596723075aa702f2346
Adson <adsonfr.vieira@hotmail.com>
2024-01-03 22:09:06 -0300

    dtos

---
+++ backLimpPlas/src/main/java/com/back_LimpPlast/dto/ClientesDTO.java
@@ -0,0 +1,29 @@
+package com.back_LimpPlast.dto;
+
+public class ClientesDTO {
+	
+	private String nome;
+	private String email;
+	private int telefone;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+
+}
