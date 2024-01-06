102e84a0b6cc5608f5daa7761dcd2ca23bc3238b
Adson <adsonfr.vieira@hotmail.com>
2024-01-05 21:27:29 -0300

    E S DTs

---
+++ backLimpPlas/src/main/java/com/back_LimpPlast/controller/dto/saidaDTO/saidaClienteDTO.java
@@ -0,0 +1,5 @@
+package com.back_LimpPlast.controller.dto.saidaDTO;
+
+public class saidaClienteDTO {
+
+}
---
+++ backLimpPlas/src/main/java/com/back_LimpPlast/controller/entradaDTO/entradaClienteDTO.java
@@ -0,0 +1,5 @@
+package com.back_LimpPlast.controller.entradaDTO;
+
+public class entradaClienteDTO {
+
+}
